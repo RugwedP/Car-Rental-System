49d697a7ffaef7f83f003c7a9e68acd037148bf6
Nivetha <nivisw04@gmail.com>
2024-12-26 21:30:45 +0530

    Update EmployeeRepository.java

--- Backend/EmployeeAutomationSystem/src/main/java/com/pms/repository/EmployeeRepository.java
+++ Backend/EmployeeAutomationSystem/src/main/java/com/pms/repository/EmployeeRepository.java
@@ -1,13 +1,12 @@
 package com.pms.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.pms.model.Employee;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {	
-		Optional<Employee> findByEmployeeEmail(String email);
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Optional<Employee> findByEmployeeEmail(String email);
 }
