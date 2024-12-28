4edcb2a7c3a1a060077a2caaf6e3cf2d28975937
Lakuleshwari <uduthalakuleshwari@gmail.com>
2024-12-28 12:28:05 +0530

    deactivating temporary employees

--- Backend/EmployeeAutomationSystem/src/main/java/com/pms/repository/EmployeeRepository.java
+++ Backend/EmployeeAutomationSystem/src/main/java/com/pms/repository/EmployeeRepository.java
@@ -3,10 +3,18 @@ package com.pms.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.pms.model.Employee;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
-
+import java.time.LocalDate; 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     Optional<Employee> findByEmployeeEmail(String email);
+    @Modifying
+	    @Transactional
+	    
+		 @Query("UPDATE Employee e SET e.status = 'inactive' WHERE e.accountExpiryDate < :currentDate AND e.status = 'active'")
+		    int deactivateExpiredEmployees(@Param("currentDate") LocalDate currentDate);
 }
