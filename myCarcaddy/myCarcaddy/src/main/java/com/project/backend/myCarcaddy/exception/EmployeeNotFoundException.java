943b9101776763f118bb21a3761acff68270b845
Nivetha <nivisw04@gmail.com>
2025-01-13 19:38:27 +0530

    sprint 2 Frontend updated code

---
+++ myCarcaddy/myCarcaddy/src/main/java/com/project/backend/myCarcaddy/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.backend.myCarcaddy.exception;
+
+public class EmployeeNotFoundException extends RuntimeException  {
+	 public EmployeeNotFoundException(String message) {
+	        super(message);
+	    }
+}
---
+++ myCarcaddy/myCarcaddy/src/main/java/com/project/backend/myCarcaddy/model/RentBooking.java
@@ -0,0 +1,110 @@
+package com.project.backend.myCarcaddy.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
+public class RentBooking {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookingId;
+	
+	
+    private String startDate;
+    private String endDate;
+    private double totalFare;
+    private int discount;
+    private String location;
+    private String status;
+    private int days;
+	
+    private int customerId;
+    private int carId;
+    
+    
+    
+//    private int employeeId;
+    @JoinColumn(name = "emp_id")
+    @ManyToOne
+    @JsonIgnoreProperties("rentBookings")
+    private Employee employee;
+    
+    
+	public int getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public double getTotalFare() {
+		return totalFare;
+	}
+	public void setTotalFare(double totalFare) {
+		this.totalFare = totalFare;
+	}
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getDays() {
+		return days;
+	}
+	public void setDays(int days) {
+		this.days = days;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public int getCarId() {
+		return carId;
+	}
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+    
+    
+    
+}
