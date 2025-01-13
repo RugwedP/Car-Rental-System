e922cfbd61dbb604d8c84ec322755f1ec7f9775a
Nivetha <nivisw04@gmail.com>
2025-01-13 19:34:03 +0530

    Frontend sprint 2 code updated

---
+++ carrentalsystem/carrentalsystem/src/main/java/com/project/carrentalsystem/CarrentalsystemApplication.java
@@ -0,0 +1,21 @@
+package com.project.carrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CarrentalsystemApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentalsystemApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
