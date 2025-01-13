fc89c779344ac4a12c01bca4329f25fb396b93e1
Nivetha <nivisw04@gmail.com>
2025-01-13 19:40:38 +0530

    Delete myCarcaddy/myCarcaddy directory

--- myCarcaddy/myCarcaddy/src/main/java/com/project/backend/myCarcaddy/exception/AdminAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.project.backend.myCarcaddy.exception;
-
-public class AdminAlreadyExistException extends RuntimeException {
-	public AdminAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- myCarcaddy/myCarcaddy/src/main/java/com/project/backend/myCarcaddy/exception/PasswordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.project.backend.myCarcaddy.exception;
-
-public class PasswordNotFoundException extends RuntimeException {
-	public PasswordNotFoundException(String message) {
-        super(message);
-    }
-}
